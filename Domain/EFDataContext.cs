f258df6970a47776efbf87ef8eb695030110af30
Marlon Gonzalez <marlongonzalezdev@gmail.com>
2020-03-14 21:31:31 -0300

    Add project files.

---
+++ Domain/EFDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain
+{
+    public class EFDataContext: DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = LearningEFCore";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ Domain/Entities/Samurai.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Clan Clan { get; set; }
+        public List<Quote> Quotes { get; set; }
+
+        public Samurai()
+        {
+            Quotes = new List<Quote>();
+        }
+    }
+}
