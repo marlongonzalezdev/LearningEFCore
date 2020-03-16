deb2e44f297762d4a2bebd81bcd9e394978f036d
Marlon Gonzalez <marlongonzalezdev@gmail.com>
2020-03-16 00:28:43 -0300

    Adding fliters example

--- Domain/EFDataContext.cs
+++ Domain/EFDataContext.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
 
 namespace Domain
 {
-    public class EFDataContext: DbContext
+    public class EFDataContext : DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Clan> Clans { get; set; }
@@ -14,13 +16,27 @@ namespace Domain
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             const string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = LearningEFCore";
-            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(connectionString);
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId});
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
             modelBuilder.Entity<Horse>().ToTable("Horses");
         }
+
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter((category, level) =>
+                        category == DbLoggerCategory.Database.Command.Name
+                        && level == LogLevel.Information)
+                    .AddConsole();
+            });
     }
 }
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -3,17 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Presentation
 {
     class Program
     {
         private static EFDataContext _context = new EFDataContext();
-        
+
         static void Main(string[] args)
         {
-            AddSamurai();
-            GetSamurais("Samurais:");
+            //AddSamurai();
+            InsertVariousTypes();
+            //GetSamurais("Samurais:");
+            QueryFilters();
             Console.ReadKey();
         }
 
@@ -25,6 +28,27 @@ namespace Presentation
             });
 
             _context.SaveChanges();
+        }
+
+        private static void InsertVariousTypes()
+        {
+            var samurai = new Samurai { Name = "Date Masamune" };
+
+            var clan = new Clan { Name = "Soul Rebels" };
+
+            _context.AddRange(samurai, clan);
+            _context.SaveChanges();
+        }
+
+        private static async void QueryFilters()
+        {
+            var name = "Date";
+            var filter = "%e";
+            var samurais =  await _context.Samurais.Where(s => EF.Functions.Like(s.Name, filter)).ToListAsync();
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
 
         private static void GetSamurais(string text)
