c64637dd904f55487f1e27f88d7b0b39ba421c43
Marlon Gonzalez <marlongonzalezdev@gmail.com>
2020-03-15 22:52:05 -0300

    Adding Console app

---
+++ Presentation/Program.cs
@@ -0,0 +1,40 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Entities;
+
+namespace Presentation
+{
+    class Program
+    {
+        private static EFDataContext _context = new EFDataContext();
+        
+        static void Main(string[] args)
+        {
+            AddSamurai();
+            GetSamurais("Samurais:");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            _context.Samurais.Add(new Samurai()
+            {
+                Name = "Toyotomi Hideyoshi"
+            });
+
+            _context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            Console.WriteLine(text);
+            var samurais = _context.Samurais.ToList();
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
+        }
+    }
+}
